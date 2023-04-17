0f2a47e9acdf7fb8ae9d84c5e98e89fa283c5353
yangzhongxiao <zhongxiao.yang@yunfangdata.com>
2023-04-17 17:31:45 +0800

    调整一下docker

--- src/openai.ts
+++ src/openai.ts
@@ -61,12 +61,12 @@ async function dalle(username:string,prompt: string) {
     response_format: CreateImageRequestResponseFormatEnum.Url,
     user: username
   }
-  // ,{
-  //   proxy:{
-  //     host:'127.0.0.1',
-  //     port:7890
-  //   }
-  // }
+  ,{
+    // proxy:{
+    //   host:'127.0.0.1',
+    //   port:7890
+    // }
+  }
   ).then((res) => res.data).catch((err) => console.log(err));
   if (response) {
     return response.data[0].url;
